33a38d2aebb2c77c322fb6752b0ec447d546587a
mici <76863251+miciella@users.noreply.github.com>
2022-05-19 17:03:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 let randomNumber1 = Math.floor((Math.random()*6)+1)
 
-let randomDiceImage = "images/dice"+randomNumber1+".png"
+let randomDiceImage = "dice"+randomNumber1+".png"
 let img1 = document.querySelectorAll("img")[0]
 img1.setAttribute("src", randomDiceImage)
 
 let img2 = document.querySelectorAll("img")[1]
 let randomNumber2 = Math.floor((Math.random()*6)+1)
-let randomDiceImage2 = "images/dice"+randomNumber2+".png"
+let randomDiceImage2 = "dice"+randomNumber2+".png"
 img2.setAttribute("src", randomDiceImage2)
 
 let getTag = document.querySelector("h1")
